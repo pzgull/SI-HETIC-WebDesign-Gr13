d547a8ebacc234ee2e6cd09b43dba16f40cafbca
Pierre Dimitrou <dimitriou.pierrea@gmail.com>
2016-05-19 14:47:20 +0200

    Add script for .search-module

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
   'use strict';
 
-  // JS Code
+  var tabitems = ['modele','immat','cartegrise'];
+
+  tabitems.forEach(function(category) {
+    $('#' + category).on('click', function() {
+      $('.search-module').trigger('clear');
+      $(this).addClass('selected');
+      $('.form-' + category).addClass('active');
+    });
+  });
+
+  $('.search-module').on('clear', function() {
+    $(this).find('ul')
+    .children('li').each(function() {
+      $(this).removeClass('selected');
+    });
+    $(this).find('.display')
+    .children('form').each(function(elt) {
+      $(this).removeClass('active');
+    })
+  });
 
 });
